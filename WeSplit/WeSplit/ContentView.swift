06d4ac103b99eb76b1cffcd0760b5cd30f991cfe
galih <samudragalih94@gmail.com>
2023-12-27 19:40:36 +0700

    feat(UI): update follow challange 1 - change title section "Amount after tips:" to "Amount per person" - add total section - change select tips section from segmented to navigation link and show percentage from 0 - 100

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,12 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkedAmount = 0.0
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 0
     @FocusState private var amountIsFocused: Bool
     
     let MIN_NUM_PEOPLE = 2
-    let tipPercentages = [0, 10, 15, 20, 25]
     var totalPerPerson: Double {
         let percentageSelected = Double(tipPercentage)
         let peopleCount = Double(numberOfPeople + MIN_NUM_PEOPLE)
@@ -23,6 +22,13 @@ struct ContentView: View {
         let grandTotal = checkedAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
         return amountPerPerson
+    }
+    
+    var total: Double {
+        let percentageSelected = Double(tipPercentage)
+        let tipValue = (checkedAmount / 100.0 * percentageSelected)
+        let grandTotal = checkedAmount + tipValue
+        return grandTotal
     }
     
     var body: some View {
@@ -42,15 +48,19 @@ struct ContentView: View {
                 }
                 Section("How much do you want to tip?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text($0, format: .percent)
+                        ForEach(0..<101) {
+                            Text("\($0)%")
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.navigationLink)
                 }
                 
-                Section("Amount after tips:") {
+                Section("Amount per person") {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "IDR"))
+                }
+                
+                Section("Total Amount") {
+                    Text(total, format: .currency(code: Locale.current.currency?.identifier ?? "IDR"))
                 }
             }
             .navigationTitle("WeSPlit")
